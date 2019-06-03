2e038ee78fca36b8b1019861378f0a17cfb1f9da
mahmoudzeyada <mahmoudzeyada440@gmail.com>
2019-06-03 22:56:26 +0200

    adding url to csvfile

--- Pastbin/urls.py
+++ Pastbin/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
 
 from api.views import DocumentViewSet
-from shortner.views import UrlRedirectView, DocumentView
+from shortner.views import UrlRedirectView, DocumentView, CsvView
 
 
 router = DefaultRouter()
@@ -15,6 +15,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include(router.urls)),
     path("documents/<int:pk>/", DocumentView.as_view()),
+    path("csv/", CsvView.as_view()),
     re_path(r'^auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
     re_path(r'^(?P<shortcode>[\w-]+)/$', UrlRedirectView.as_view()),
