070c2fd52fcb22bb78a0d890343b78cb74da9148
mahmoudzeyada <mahmoudzeyada440@gmail.com>
2019-06-03 22:59:21 +0200

    modyfing settings file

--- Pastbin/settings.py
+++ Pastbin/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Pastbin.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Cairo'
 
 USE_I18N = True
 
