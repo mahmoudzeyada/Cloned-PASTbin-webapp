a0d19057973b7cd04eeb1f704db4bb755d50c6d7
mahmoudzeyada <mahmoudzeyada440@gmail.com>
2019-06-03 21:08:18 +0200

    removing filed and logic does not make sense

--- shortner/models.py
+++ shortner/models.py
@@ -8,12 +8,9 @@ SHORT_URL_MAX = getattr(settings, "SHORT_URL_MAX ", 15)
 
 
 class CustomManager(models.Manager):
-    def all(self, *args, **kwargs):
-        qs = super().all(*args, **kwargs)
-        return qs.filter(active=True)
 
     def refresh_all_shortcode(self, items=None):
-        qs = UrlShortener.objects.filter(id__gte=1)
+        qs = UrlShortener.objects.all()
         if items is not None or isinstance(items, int):
             qs = qs.order_by("-id")[:items]
         new_code = 0
@@ -32,14 +29,14 @@ class UrlShortener(models.Model):
         max_length=SHORT_URL_MAX, unique=True, blank=True)
     created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now_add=True)
-    active = models.BooleanField(default=True)
 
     objects = CustomManager()
 
     def save(self, *args, **kwargs):
         if self.shortcode is None or self.shortcode == "":
             self.shortcode = create_shortcode(self)
-            self.url = "http://127.0.0.1:8000"+self.document.id
+            self.url =\
+                "http://127.0.0.1:8000/documents/"+str(self.document.id)
         super().save(*args, **kwargs)
 
     def __str__(self):
