cb51774b76353d3c51dc90215e01a858dc3d9873
mahmoudzeyada <mahmoudzeyada440@gmail.com>
2019-06-03 21:07:23 +0200

    adding url redirect and detail view

--- Pastbin/urls.py
+++ Pastbin/urls.py
@@ -4,7 +4,8 @@ from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
 
 from api.views import DocumentViewSet
-from shortner.views import UrlRedirectView
+from shortner.views import UrlRedirectView, DocumentView
+
 
 router = DefaultRouter()
 router.register("documents", DocumentViewSet, base_name="documents")
@@ -13,7 +14,8 @@ router.register("documents", DocumentViewSet, base_name="documents")
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include(router.urls)),
+    path("documents/<int:pk>/", DocumentView.as_view()),
     re_path(r'^auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
-    re_path(r'(?P<short_code>[\w-]+)/$', UrlRedirectView.as_view()),
+    re_path(r'^(?P<shortcode>[\w-]+)/$', UrlRedirectView.as_view()),
 ]
--- shortner/views.py
+++ shortner/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.views.generic import DetailView
+
+from .models import UrlShortener
+from api.models import Document
 
 
 class UrlRedirectView(View):
+    '''View for retriving the actual  url'''
+
     def get(self, request, *args, **kwargs):
-        return HttpResponse("hello")
+        document = get_object_or_404(
+            UrlShortener, shortcode=kwargs.get('shortcode'))
+        document_url = document.url
+        return HttpResponseRedirect(document_url)
+
+
+class DocumentView(DetailView):
+    context_object_name = 'document'
+    model = Document
+    template_name = "documents/document_details.html"
