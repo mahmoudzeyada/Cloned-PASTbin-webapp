d6f7d22061ca0e8c39c6584156296fa7c41f2fef
mahmoudzeyada <mahmoudzeyada440@gmail.com>
2019-06-03 22:55:53 +0200

    adding api models to admin site

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Document, SharedDocuments
+
+admin.site.register(Document)
+admin.site.register(SharedDocuments)
